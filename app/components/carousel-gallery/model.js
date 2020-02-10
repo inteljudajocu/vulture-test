d5c5a5398bc7fd016c244f30b4c446e821597530
Juan <jjoa@intenllisys.com.do>
2020-02-10 16:27:00 -0400

    carousel with elastic integrated

---
+++ app/components/carousel-gallery/model.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const { search } = require('../../services/server/elastic'),
+  query = {
+    _source: ['image'],
+    size: 4,
+    from: 1,
+    sort: [{ date: 'desc' }],
+    query: {
+      match_all: {}
+    }
+  };
+
+function getArticleElastic(data) {
+  return search('local_articles', query)
+    .then(({ hits }) => hits.hits)
+    .then(hits => hits.map(({ _source }) => _source))
+    .then(res => {
+      data.gallery = res;
+
+      return data;
+    });
+}
+
+module.exports.render = function(uri, data) {
+  return getArticleElastic(data).then(data => data);
+};
