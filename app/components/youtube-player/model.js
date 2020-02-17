a4a2d8e5188962691f2f69b1927d91a66b78b4cf
Juan <jjoa@intenllisys.com.do>
2020-02-17 12:06:57 -0400

    video output from elastic

--- app/components/youtube-player/model.js
+++ app/components/youtube-player/model.js
@@ -1,13 +1,37 @@
 'use strict';
 
+const { search } = require('../../services/server/elastic'),
+  query = {
+    query: {
+      match_all: {}
+    }
+  };
+
 function videoPlayer(videoSrc) {
   if (videoSrc != '') return videoSrc.replace('watch?v=', 'embed/');
   return videoSrc;
 }
 
-module.exports.render = function(uri, data) {
+module.exports.render = function(uri, data, local) {
   let { videoSrc } = data;
 
   data.videoSrc = videoPlayer(videoSrc);
-  return data;
+
+  return getVideoElastic(data, local.params.name).then(data => data);
 };
+
+function getVideoElastic(data, pageName) {
+  return search('local_video', query)
+    .then(({ hits }) => hits.hits)
+    .then(hits => {
+      let { _id, _source } = hits[0],
+        temp;
+
+      return (temp = { id: _id, video: _source.videoUrl });
+    })
+    .then(res => {
+      if (res.id.includes(pageName)) data.videoSrc = res.video;
+
+      return data;
+    });
+}
