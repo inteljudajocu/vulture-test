f049093a39bc2c96ec9c68a93441e1fef4a9918f
Juan <jjoa@intenllisys.com.do>
2020-02-20 10:05:00 -0400

    side ggit add .

--- app/components/side-gallery/model.js
+++ app/components/side-gallery/model.js
@@ -1,1 +1,36 @@
 'use strict';
+
+const { search } = require('../../services/server/elastic');
+
+module.exports.render = function(uri, data, local) {
+  let name = 'a';
+
+  if (local.params == null) name = local.url;
+  else name = local.params.name;
+
+  return getGalleryElastic(data, name).then(data => data);
+};
+
+function getGalleryElastic(data, name) {
+  const query = {
+    size: 1,
+    query: {
+      bool: {
+        must: {
+          query_string: {
+            query: name,
+            default_field: 'internalUrl'
+          }
+        }
+      }
+    }
+  };
+
+  return search('video', query)
+    .then(({ hits }) => hits.hits)
+    .then(hits => hits.map(({ _source }) => _source))
+    .then(respond => {
+      data.gallery = respond;
+      return data;
+    });
+}
