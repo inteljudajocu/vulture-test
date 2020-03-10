50a6d765ef597b91967e4cc0ec8aa19be5256cc8
Juan <jjoa@intenllisys.com.do>
2020-03-10 10:29:46 -0400

    rendering page, applying validator feedback

--- app/components/feed/model.js
+++ app/components/feed/model.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const { rawQuery } = require('../../services/server/querys'),
-  index = 'articles';
+  index = 'articles',
+  descp =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean egestas dapibus justo, at hendrerit nisl tincidunt vel. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla gravida luctus justo, quis sagittis dolor porttitor et. Pellentesque consectetur dolor eget porta elementum. Morbi at convallis nibh, vel viverra quam. Nunc aliquam blandit sodales. Donec tempor vehicula arcu. Etiam id fermentum risus, eget consectetur augue. Cras ornare lacinia purus a dictum. Suspendisse et mi in ex vulputate maximus consectetur vitae arcu. Duis iaculis metus non felis mollis auctor. Pellentesque orci massa, ultrices quis lorem et, iaculis consectetur arcu. Maecenas sagittis nisi et libero tincidunt condimentum. Cras molestie augue in enim laoreet malesuada.';
 
 function rawArticle(data) {
   return rawQuery(index).then(res => {
@@ -13,7 +15,8 @@ function rawArticle(data) {
         url: element.url,
         date: element.date,
         author: element.author,
-        image: element.image
+        image: element.image,
+        description: descp
       };
 
       articles.push(item);
--- app/components/feed/rss.model.js
+++ app/components/feed/rss.model.js
@@ -1,10 +1,45 @@
 'use strict';
 
-const xml = require('xml'),
-  amphoRss = require('amphora-rss');
+function addEntrys(data) {
+  const { articles } = data,
+    items = [],
+    feed = [];
 
-module.exports.render = function(uri, data) {
-  console.log(data);
+  articles.forEach(element => {
+    let item = [
+      { title: element.title },
+      { a: element.url },
+      { date: element.date },
+      { author: element.author },
+      { img: element.image },
+      { description: element.description },
+      { category: 'rss' }
+    ];
+
+    items.push(item);
+  });
+  delete data.articles;
+  return (data.feed = items);
+}
+
+function addMeta(data) {
+  return (data.meta = {
+    title: 'rss',
+    description: 'rss format',
+    link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+  });
+}
+
+function addAttr(data) {
+  return (data.attr = {});
+}
+
+module.exports = function(uri, data) {
+  addMeta(data);
+  addEntrys(data);
+  addAttr(data);
+
+  console.log('This is data HIII >>>>>> ', data);
 
   return data;
 };
